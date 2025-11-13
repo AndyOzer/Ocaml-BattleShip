957af7c33d4ebf607a8858ed596330020eb1c9ec
root <root@HopWINS-CT-Andy.local>
2025-11-13 03:36:31 +0000

    Update website routing structure

---
+++ src/lib/web_ui.mli
@@ -0,0 +1,5 @@
+val hdlr_index : Dream.request -> Dream.response Lwt.t
+(* Web GET request handler for route "/" *)
+
+val hdlr_game : Dream.request -> Dream.response Lwt.t
+(* Web GET request handler for route "/game" *)
